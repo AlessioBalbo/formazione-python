823ca8cb0548385cb29f3182fed8e93d41a9d350
Alessio Balbo <alessio@develer.com>
2020-10-30 14:10:24 +0100

    added cat command

--- TCP-socket/TCPsocket2server.py
+++ TCP-socket/TCPsocket2server.py
@@ -4,6 +4,11 @@ import socket
 import sys
 import subprocess
 
+help_msg = ("command list: \n - print <msg>: echo <msg>\n" 
+            " - ls: list files\n"
+            " - cat <file1>...<fileN>: concatenate files\n"
+            " - help: show command lists\n")
+
 HOST = ''
 PORT = 50000
 
@@ -22,9 +27,12 @@ if __name__ == "__main__":
         if data is None: break
         print(data)
         command = data.split()[0]
-        if command == 'echo':
+        if command == 'print':
+            echo_msg=[]
+            for elem in data.split()[1:]:
+                echo_msg.append(elem)
             if len(data.split()) > 1:
-                process = subprocess.Popen(['echo', data.split()[1]],
+                process = subprocess.Popen(['echo', echo_msg],
                                      stdout = subprocess.PIPE,
                                      stderr = subprocess.PIPE)
                 stdout, stderr = process.communicate()
@@ -42,8 +50,18 @@ if __name__ == "__main__":
             ##    response += str(elem)
             ##    response += "\n"
             ##conn.send(response.encode())
+        elif command == 'cat':
+            cat_cmd=[]
+            for elem in data.split():
+                cat_cmd.append(elem)
+            process = subprocess.Popen(cat_cmd,
+                                    stdout = subprocess.PIPE,
+                                    stderr = subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            conn.send(stdout)
+            conn.send(stderr)
         else:
-            conn.send("help".encode()) 
+            conn.send(help_msg.encode()) 
         ##conn.close()
         
 
